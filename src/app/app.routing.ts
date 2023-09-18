8b3b5a41d9aa8d79ee5a98e0350e4bbaa9bcee86
fmanuelm <fmanuelm2012@gmail.com>
2023-09-18 16:20:07 -0500

    Agregando los guards las otras rutas

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -41,39 +41,50 @@ export const AppRoutes: Routes = [
                 loadChildren: () => import('./calendar/calendar.module').then(m => m.CalendarModule)
             }, {
                 path: 'clientes',
-                loadChildren: () => import('./clients/clients.module').then(m => m.ClientsModule)
+                loadChildren: () => import('./clients/clients.module').then(m => m.ClientsModule),
+                canActivate: [AuthGuard]
             }, {
                 path: 'clientes-grupos',
-                loadChildren: () => import('./clientsgroups/clientsgroups.module').then(m => m.ClientsgroupsModule)
+                loadChildren: () => import('./clientsgroups/clientsgroups.module').then(m => m.ClientsgroupsModule),
+                canActivate: [AuthGuard]
             },
             {
                 path: 'disponibilidad-usuario',
-                loadChildren: () => import('./usersavailability/usersavailability.module').then(m => m.UsersavailabilityModule)
+                loadChildren: () => import('./usersavailability/usersavailability.module').then(m => m.UsersavailabilityModule),
+                canActivate: [AuthGuard]
             },
             {
                 path: 'contratos',
-                loadChildren: () => import('./contrats/contrats.module').then(m => m.ContratsModule)
+                loadChildren: () => import('./contrats/contrats.module').then(m => m.ContratsModule),
+                canActivate: [AuthGuard]
             }, {
                 path: 'sucursal-instalacion',
-                loadChildren: () => import('./branch-facility/branch-facility.module').then(m => m.BranchFacilityModule)
+                loadChildren: () => import('./branch-facility/branch-facility.module').then(m => m.BranchFacilityModule),
+                canActivate: [AuthGuard]
             }, {
                 path: 'equipos',
-                loadChildren: () => import('./equipment/equipment.module').then(m => m.EquipmentModule)
+                loadChildren: () => import('./equipment/equipment.module').then(m => m.EquipmentModule),
+                canActivate: [AuthGuard]
             }, {
                 path: 'puntos',
-                loadChildren: () => import('./points/points.module').then(m => m.PointsModule)
+                loadChildren: () => import('./points/points.module').then(m => m.PointsModule),
+                canActivate: [AuthGuard]
             }, {
                 path: 'monitor-equipos',
-                loadChildren: () => import('./equipment-monitor/equipment-monitor.module').then(m => m.EquipmentMonitorModule)
+                loadChildren: () => import('./equipment-monitor/equipment-monitor.module').then(m => m.EquipmentMonitorModule),
+                canActivate: [AuthGuard]
             }, {
                 path: 'grupos-puntos',
-                loadChildren: () => import('./points-groups/points-groups.module').then(m => m.PointsGroupsModule)
+                loadChildren: () => import('./points-groups/points-groups.module').then(m => m.PointsGroupsModule),
+                canActivate: [AuthGuard]
             }, {
                 path: 'usuarios',
-                loadChildren: () => import('./users/users.module').then(m => m.UsersModule)
+                loadChildren: () => import('./users/users.module').then(m => m.UsersModule),
+                canActivate: [AuthGuard]
             }, {
                 path: 'mensajes',
-                loadChildren: () => import('./messages/messages.module').then(m => m.MessagesModule)
+                loadChildren: () => import('./messages/messages.module').then(m => m.MessagesModule),
+                canActivate: [AuthGuard]
             }, {
                 path: '',
                 loadChildren: () => import('./userpage/user.module').then(m => m.UserModule)
